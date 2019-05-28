952aaf88c025c828f7b221482688959ec5671964
Farhan Sadik <farhansadikriduan@gmail.com>
2019-05-28 14:58:40 +0600

    updated echominal @ 1.0.3

---
+++ .engine.bash
@@ -0,0 +1,324 @@
+#!/bin/bash
+#!/system/bin/bash
+
+set import busybox 
+set import bash
+set import netstat 
+set import whois 
+set import p7zip
+
+script_version="1.1.17"
+engine_version="1.0.3 Lapato"
+
+####################################################################
+# Farhan Sadik (squaredevops.blogspot.com) - May 2019
+####################################################################
+
+# Define variables
+red='\033[1;91m'; deep_green='\033[0;32m'; green='\033[1;92m'; yellow='\033[1;93m'; blue='\033[1;94m'; white='\033[1;97m'; stop='\e[0m';
+engine=~/./.engine.bash; # the engine modified 
+zsh_color='/data/data/com.termux/files/home/.termux/colors.sh';
+zsh_font='/data/data/com.termux/files/home/.termux/fonts.sh';
+zsh_theme=~/.oh-my-zsh/tools/theme_chooser.sh;
+zsh_update=~/.oh-my-zsh/tools/upgrade.sh;
+zsh_uninstall=~/.oh-my-zsh/tools/uninstall.sh;
+
+# log files
+log=~/.log/system-status.log; # for track logs 
+hacking_install_log=~/.log/hacking_install.log;
+hacking_running_log=~/.log/hacking_running.log;
+
+# shell-setting [in test]
+user_name=~/.config/echominal/user_name.cfg;
+pc_name=~/.config/echominal/pc_name.cfg;
+shell_style=~/.config/echominal/shell_style.cfg;
+
+# Purpose: Display pause prompt
+function pause(){
+	local message="$@"
+	[ -z $message ] && message="Press [Enter] key to continue..."
+	read -p "$message" readEnterKey
+}
+function write_header(){
+	local h="$@"
+	echo "------------------------------------------------------";
+	echo " ${h}";
+	echo "------------------------------------------------------";
+}
+function disk_info() {
+	usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1)
+	  partition=$(echo $output | awk '{print $2}')
+	write_header " Disk Usage Info"
+	if [ "$EXCLUDE_LIST" != "" ] ; then
+	  df | grep -vE "^Filesystem|tmpfs|cdrom|${EXCLUDE_LIST}" | awk '{print $5 " " $6}'
+	  #df -H | grep -vE "^Filesystem|tmpfs|cdrom|${EXCLUDE_LIST}" | awk '{print $5 " " $6}'
+	else
+	  df | grep -vE "^Filesystem|tmpfs|cdrom" | awk '{print $5 " " $6}'
+	  #df -H | grep -vE "^Filesystem|tmpfs|cdrom" | awk '{print $5 " " $6}'
+	fi
+}
+function input() { 
+	echo; echo -n "`cat $shell_style`" && read input; echo;  	# modified
+	# echo; echo -n "`whoami`@linux ~> " && read input; echo; 	# orgional
+	# read -p "`cat $shell_style`" shell 						# tred 
+}
+function footer() {
+	printf "$deep_green" && printf "EchoMinal $engine_version\nCreated by Farhan Sadik\n $stop";
+	echo && exit 0;
+}
+function show_menu(){
+	clear && printf "$red" && figlet "EchoMinal"; printf "$stop"
+	#printf "$blue" && printf "Termux Manager for Android OS $stop \n" # && echo
+	printf "$deep_green" && printf "Termux Manager for Android OS\n" # && echo
+	#printf "$red" && printf "Termux Manager for Android OS\n" # && echo
+	#printf "$yellow" && printf "Termux Manager for Android OS $stop \n" # && echo
+	printf "\n$stop"
+	printf "  [1]  Terminal\n";
+	printf "  [2]  Hacking Scripts\n";
+	printf "  [3]  Tools & Utilities\n";
+	printf "  [4]  System Maintanance\n";
+	printf "  [5]  About\n";
+	printf "  [0]  Exit\n";
+}
+function terminal() {
+	# this is terminal menu 
+	echo; printf "  [1]  Fish\n"; printf "  [2]  Zsh\n"; printf "  [0]  Back\n"; 
+	input && if [[ $input == 1 ]] ; then fish # fish 3.0.2-1 (arch-linux) 
+		elif [[ $input == 2 ]]; then zsh # zsh 5.7.1-1 (arch-linux) 
+		elif [[ $input == 0 ]]; then $engine # run the engine again 
+		else printf "Wrong Input\n"; 
+	fi; #pause
+}
+function hacking_script() {
+	
+	#########################################################################
+	# Script Menager 
+	# UNDER DEVELOPMENT - CROSSLINE
+	#########################################################################
+ 
+	printf "\n$red|-[ NOTE ] -------------------------------|\n| This function is under development      |\n| Please wait for next release            |\n|_________________________________________|\n$stop"; 
+	echo; # extra echo
+	printf "  [01] Install all scripts\n"; # install all 
+	printf "  [02] Weeman\n";  # run weeman script
+	printf "  [00] Exit\n";
+
+	input; # echo; # those data will store in $HOME/.scripts/
+	case $input in 
+		01)	# Install all of scripts 
+			printf "$red";
+			printf "[*] Changing Location	: "; if cd ~/.scripts/; then printf "OK\n"; fi;
+			printf "[*] Checking Location	: "; if pwd > $hacking_install_log; then printf "OK\n"; fi;
+			printf "[*] Checking File       : "; if ls >> $log; then printf "OK\n"; fi;
+			printf "Ready to run.....\n $stop";
+
+			# installing weeman script 
+			echo && read -p "want to install weeman? (y/n) " WINSTALL; 
+			if [[ $WINSTALL == 'y' ]]; then {
+					echo -e "  \e[5mInstalling Weeman Script\e[0m"
+					if git clone https://github.com/evait-security/weeman; then printf "Compleate\n"; else printf "Failed\n"; fi;
+				}
+			elif [[ $WINSTALL == 'n' ]]; then { 
+				sleep 0.5
+			}; else {
+				echo "Invalid Options"
+			}; fi; 
+			
+			# printf "[*] Getting Permission	: "; if chmod +x $file; then printf "OK\n"; fi;
+		;;
+		02) # Weeman
+			printf "$red\n"; sleep 0.5
+			printf "[*] Changing Directory	: "; if cd ~/.scripts/weeman/; then printf "OK\n"; fi;
+			printf "[*] Checking Location	: "; if pwd > $hacking_running_log; then printf "OK\n"; fi;
+			printf "[*] Checking FileSystem : "; if ls >> $log; then printf "OK\n"; fi;
+			printf "[*] Getting Permission	: "; if chmod +x weeman.py; then printf "OK\n"; fi;
+			printf "Ready to run.....\n $stop"; sleep 1 
+			./weeman.py # running
+		;;
+		00) 
+			sleep 0;  $engine
+		;;
+		*) 
+			printf "Wrong Input\n"; pause
+		;;
+	esac; # pause 
+}
+function tools() {
+	# tools_and_utilities function
+	printf "\n  [1] Htop [Monitoring Tools]\n"; 
+	printf "  [2] Vim [Text Editor]\n";
+	printf "  [3] Nano [Text Editor]\n"; 
+	printf "  [4] Python\n"; 
+	printf "  [5] Linux SystemAdmin Tool\n";
+	printf "  [6] Zip Archiver\n";
+	printf "  [7] ISO Compression\n";
+	printf "  [8] Whois Tool\n";
+	printf "  [0] Back\n";
+	input; if [[ $input == 1 ]]; then htop;
+	elif [[ $input == 2 ]]; then vim;
+	elif [[ $input == 3 ]]; then nano;
+	elif [[ $input == 4 ]]; then python;
+	elif [[ $input == 5 ]]; then bash ~/.scripts/SysAdmin.bash;
+	elif [[ $input == 6 ]]; then bash ~/.scripts/zipc.bash;
+	elif [[ $input == 7 ]]; then bash ~/.scripts/isoc.bash;
+	elif [[ $input == 8 ]]; 
+		then {
+			read -p "enter address : " address; whois $address; pause
+		}
+	elif [[ $input == 0 ]]; then $engine; # run the engine again 
+	else {
+		printf "Invalid Options\n" && echo; # footer;
+	}; fi; # pause; 
+}
+function input_settings() {
+    printf "\n$deep_green" && printf "Please enter your short name\n$stop\n";
+    read -p "enter user name    : " user && echo $user > $user_name;
+    read -p "enter pc/host name : " pc && echo $pc > $pc_name;
+    
+    printf "\n$deep_green" && printf "Please select your style. This is how it should look like. \n \n";
+    printf "$yellow" && printf "[1] Arch Linux Style $stop
+    [$red`cat $user_name`$white@$blue`cat $pc_name`$white ~] $ \n";
+    printf "$yellow" && printf "[2] Parrot Linux Style  $stop
+    ┌─[$red`cat $user_name`$white@$blue`cat $pc_name`$white]─[~]
+    └─➤ \n";
+    printf "$yellow" && printf "[3] Fish Shell Style  $stop
+    $red`cat $user_name`$white@$blue`cat $pc_name`$white ~> \n";
+    printf "$yellow" && printf "[4] Marato Style $stop
+    » $red`cat $user_name`$white@$blue`cat $pc_name`$white╺─╸[~] \n"
+    printf "$yellow" && printf "[*] Default Style $stop
+    $red`cat $user_name`$white@$blue`cat $pc_name`$white $ \n";
+
+    printf "\n$deep_green" && printf "Please select a style, 1-3. enter to default.\n$stop";
+    read -p "echominal $ " style; printf "\n$red";
+
+    if [[ $style == 1 ]]; then {
+        echo "Installing ......"; sleep 0.5;
+        if echo "[`cat $user_name`@`cat $pc_name` ~] $ " > $shell_style; then {
+            echo "Install Compleated";
+        }; fi; 
+    }; elif [[ $style == 2 ]]; then {
+        echo "Installing ......"; sleep 0.5;
+        echo "┌─[`cat $user_name`@`cat $pc_name`]─[~]
+└─➤ " > $shell_style; echo "Install Compleated";
+    }; elif [[ $style == 3 ]]; then {
+        echo "Installing ......"; sleep 0.5; echo "`cat $user_name`@`cat $pc_name` ~> " > $shell_style; echo "Install Compleated";
+    }; elif [[ $style == 4 ]]; then {
+        echo "Installing ......"; sleep 0.5; echo "» `cat $user_name`@`cat $pc_name`╺─╸[~] " > $shell_style; echo "Install Compleated";
+    }; else {
+        echo "Installing ......"; sleep 0.5; echo "Default Style Installing"; sleep 0.5; echo "`cat $user_name`@`cat $pc_name` $ " > $shell_style; echo "Install Compleated";
+    }; fi && printf "\n$stop";
+}
+function system() {
+	# System Maintanance
+	printf "\n$red";
+	printf "[1]  System Upgrade\n";
+	printf "[2]  Install Package\n";
+	printf "[3]  Uninstall Package\n";
+	printf "[4]  Operating System Info\n"; 
+	printf "[5]  Hostname & DNS Info\n";
+	printf "[6]  Network Interface\n";
+	printf "$yellow" && printf "[7]  EchoMinal Settings $stop\n";
+	printf "$red" && printf "[8]  Disk Usage\n"; 
+	printf "[9]  Reboot Device\n"; 
+	printf "[0]  Quit\n $stop";
+	
+	input && if [[ $input == 1 ]]; then 
+		# system upgrade 
+		echo && sleep 0.5; clear && figlet "SYSMD"; printf "\nChecking for updates..\n";
+		if apt update; then printf "Update Compleate\n"; else printf "failed to update, please try again\n"; fi;
+		printf "\nPreparing to system upgrade.....\n"; 
+		if pkg upgrade; then printf "System upgrade has been compleated\n"; else printf "failed to upgrade, please try again\n"; fi;
+	elif [[ $input == 2 ]]; then 
+		# install a package 
+		if apt update; then printf "Update Compleate\n"; fi; 
+		echo; if read -p "enter package name ~> " package_name; then echo && pkg install $package_name; fi; 
+	elif [[ $input == 3 ]]; then 
+		# uninstall a package 
+		echo; if read -p "enter package name ~> " package_name; then pkg remove $package_name; fi; 
+	elif [[ $input == 4 ]]; then screenfetch -n && echo ; 
+	elif [[ $input == 5 ]]; then netstat;
+	elif [[ $input == 6 ]]; then ifconfig;
+	elif [[ $input == 7 ]]; then {
+		printf "$yellow";
+		printf "  [1] ZSH Font\n";
+		printf "  [2] ZSH Color\n";
+		printf "  [3] ZSH Theme\n";
+		printf "  [4] Update ZSH\n";
+		printf "  [5] Uninstall ZSH\n";
+		printf "  [6] EchoMinal Shell Settings\n";
+		printf "  [0] Back $stop \n";
+		input && if [[ $input == 1 ]]; then {
+			$zsh_font
+		} elif [[ $input == 2 ]]; then {
+			$zsh_color;
+		} elif [[ $input == 3 ]]; then {
+			$zsh_theme;
+		} elif [[ $input == 4 ]]; then {
+			sh $zsh_update;
+		} elif [[ $input == 5 ]]; then {
+			$zsh_uninstall;
+		} elif [[ $input == 6 ]]; then {
+			input_settings;
+		} else {
+			echo "invalid";
+		}; fi;
+	}
+	elif [[ $input == 8 ]]; then disk_info;
+	elif [[ $input == 9 ]]; then echo "System Rebooting"; reboot && sleep 0.5; # root access required
+	elif [[ $input == 0 ]]; then $engine;
+	else echo "Wrong KeyWord"; fi; #pause;
+}
+function about() {
+	printf " $yellow
+EchoMinal Engine
+Termux Manager for$deep_green Android OS $yellow
+
+Engine Version   :$red $engine_version $yellow
+Script Version   :$red $script_version $yellow
+Termux           :$blue Termux v0.72 $yellow
+Linux Kernel     :$red `uname -sr` $yellow
+
+EchoMinal gives you a simple way to access 
+termux application. 
+
+contact to dev ~ farhansadikriduan@gmail.com
+blogspot ~ https://squaredevops.blogspot.com
+
+Farhan Sadik
+System Administrator 
+Square Development Group
+$stop \n"; pause 
+}
+function main(){
+	local c; echo 
+	# read -p "`whoami`@linux ~>  " c 		# orgional
+	read -p "`cat $shell_style`" c 			# new modified 
+	case $c in
+		1) terminal ;; 
+		2) hacking_script ;; 
+		3) tools ;; 
+		4) system ;;
+		5) about ;;
+		0) echo && footer ;;
+		*) 
+			echo "Please select between 1 to 5 choice only.";
+			printf "Sorry you've entered wrong options\n"; sleep 1;
+			echo -e "\e[5mPlease Wait.....\e[0m"; sleep 1
+			printf "Connecting to Database\n"; sleep 1
+			echo -e "\e[5mPlease Wait.....\e[0m"; sleep 1
+			echo -e "$red\e[7mRebooting Engine.....\e[0m" && sleep 1; 
+			# pause - enters the pause menu 
+			$engine;
+	# pause
+	esac
+}
+
+# main logic
+while true
+	do
+		show_menu # display memu
+		main # wait for user input
+done
+exit 0 
+
+# A Program By, Farhan Sadik 
+# Copyright (C) Square Development Group 
---
+++ README.md
@@ -0,0 +1,96 @@
+# Project EchoMinal
+**This script a simple manager for termux. User can use is in a short way. This script is very helpfull for beginners.
+Termux is an Android terminal emulator and Linux environment app that works directly with no rooting or setup required. A minimal base system is installed automatically - additional packages are available using the APT package manager.**
+
+**Secure** Access remote servers using the ssh client from OpenSSH. Termux combines standard packages with accurate terminal emulation in a beautiful open source solution.
+
+**Feature packed** Take your pick between Bash, fish or Zsh and nano, Emacs or Vim. Grep through your SMS inbox. Access API endpoints with curl and use rsync to store backups of your contact list on a remote server.
+
+**Customizable** Install what you want through the APT package management system known from Debian and Ubuntu GNU/Linux. Why not start with installing Git and syncing your dotfiles?
+
+**Explorable** Have you ever sat on a bus and wondered exactly which arguments tar accepts? Packages available in Termux are the same as those on Mac and Linux - install man pages on your phone and read them in one session while experimenting with them in another.
+
+**With batteries included** Can you imagine a more powerful yet elegant pocket calculator than a readline-powered Python console? Up-to-date versions of Perl, Python, Ruby and Node.js are all available.
+
+**Ready to scale up** Connect a Bluetooth keyboard and hook up your device to an external display if you need to - Termux supports keyboard shortcuts and has full mouse support.
+
+**Tinkerable** Develop by compiling C files with Clang and build your own projects with CMake and pkg-config. Both GDB and strace are available if you get stuck and need to debug.
+
+## Features
+  * Simple to use
+  * Fast and beautiful design
+  * We used material design & UX library to build this application
+  * We used MVC design pattern
+  * Password hashed
+  * ... and more
+
+## Language Used
+  * Bash Scripting
+
+## Terminal resources
+* [XTerm control sequences](http://invisible-island.net/xterm/ctlseqs/ctlseqs.html)
+* [vt100.net](http://vt100.net/)
+* [Terminal codes (ANSI and terminfo equivalents)](http://wiki.bash-hackers.org/scripting/terminalcodes)
+
+## Terminal emulators
+* VTE (libvte): Terminal emulator widget for GTK+, mainly used in gnome-terminal. [Source](https://github.com/GNOME/vte), [Open Issues](https://bugzilla.gnome.org/buglist.cgi?quicksearch=product%3A%22vte%22+), and [All (including closed) issues](https://bugzilla.gnome.org/buglist.cgi?bug_status=RESOLVED&bug_status=VERIFIED&chfield=resolution&chfieldfrom=-2000d&chfieldvalue=FIXED&product=vte&resolution=FIXED).
+* iTerm 2: OS X terminal application. [Source](https://github.com/gnachman/iTerm2), [Issues](https://gitlab.com/gnachman/iterm2/issues) and [Documentation](http://www.iterm2.com/documentation.html) (which includes [iTerm2 proprietary escape codes](http://www.iterm2.com/documentation-escape-codes.html)).
+* Konsole: KDE terminal application. [Source](https://projects.kde.org/projects/kde/applications/konsole/repository), in particular [tests](https://projects.kde.org/projects/kde/applications/konsole/repository/revisions/master/show/tests), [Bugs](https://bugs.kde.org/buglist.cgi?bug_severity=critical&bug_severity=grave&bug_severity=major&bug_severity=crash&bug_severity=normal&bug_severity=minor&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&product=konsole) and [Wishes](https://bugs.kde.org/buglist.cgi?bug_severity=wishlist&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&product=konsole).
+* hterm: JavaScript terminal implementation from Chromium. [Source](https://github.com/chromium/hterm), including [tests](https://github.com/chromium/hterm/blob/master/js/hterm_vt_tests.js), and [Google group](https://groups.google.com/a/chromium.org/forum/#!forum/chromium-hterm).
+* xterm: The grandfather of terminal emulators. [Source](http://invisible-island.net/datafiles/release/xterm.tar.gz).
+* Connectbot: Android SSH client. [Source](https://github.com/connectbot/connectbot)
+* Android Terminal Emulator: Android terminal app which Termux terminal handling is based on. Inactive. [Source](https://github.com/jackpal/Android-Terminal-Emulator).
+
+## Requirements
+  * Termux 			(download latest version from play store)
+  * Termux-Api 	(download latest version from play store)
+  * X-Plorer		(3rd Pary App) (for beginners it's very helpful) [click here to download](https://play.google.com/store/apps/details?id=com.lonelycatgames.Xplore&hl=en)
+  * Internet Connection
+  * Android Root Access
+
+### Installation Method
+> Step 1 - Internet Connection Required
+
+1. Firstly open termux app (internet connection required)
+2. After open the termux app, some of packages will be installed automated. Please do not stop, wait for finish the installation.
+3. After installation compleate, then you'll have the `bash` shall.
+
+> Step 2 - Internet Connection Required
+
+1. Update packages to latest version
+	- Update Termux using `pkg update` command.
+	- Install git using `apt install git` command.
+2. Download the repository files (project) from the download section or clone this project by typing in the bash the following command: `git clone https://gitlab.com/farhansadik/echominal.git`
+3. Then Redirect to `echominal` folder using `cd echominal`.
+4. Enter `ls` command, for check all of files directory. The output looks like ~
+  ```
+  farhansadik@linux ~ > ls
+  ChangeLog  extras.zip  install  LICENSE  README.md
+  ```
+5. Setup permission to installer `chmod +x install` and hit enter.
+6. Then run the installer sctipt `./install`. Remember that, the installer would take few more minutes and it would take about 50 megabytes of internet. This installer will needed 100 megabyte free storage on system. `(PhoneMemory / sdcard)`
+7. After install all of packages, close termux app and run it again..
+8. Hope, everything works fine. If you face any problem please mail me at `farhansadikriduan@gmail.com` or create a issue on my repo. I'll try my best to fix those issue.
+*. Thank's a lot.
+
+## How to use the Termux application
+ * wiil be added soon
+
+# Screenshots
+## EchoMinal Engine
+![EchoMinal Engine](/image/echominal_engine.png?raw=true "Engine")
+## Echominal About
+![EchoMinal About](/image/echominal_about.png?raw=true "About")
+## EchoMinal PC Engine
+![EchoMinal Engine PC](/image/echominal_pc.png?raw=true "pc")
+## Echominal Installer
+![EchoMinal Engine PC](/image/echominal_installer.png?raw=true "installer")
+## Showing Packages While Installing Echominal
+![EchoMinal Engine PC](/image/echominal_showing_packages.png?raw=true "packages")
+
+# Contributing
+If you want to contribute to this project and make it better with new ideas, your pull request is very welcomed. If you find any issue just put it in the repository issue section, thank you.
+
+**Development By** <br>
+_**Farhan Sadik**_ <br>
+**Square Development Group**
