e73f71ed9d57606a44d274a4f9d3649d41eaa8e4
Farhan Sadik <farhansadikriuuan@gmail.com>
2019-08-24 14:06:39 +0600

    updated @ 0.1.03

--- .scripts/update.bash
+++ .scripts/update.bash
@@ -3,8 +3,8 @@
 #!/system/bin/bash
 #!/data/data/com.termux/files/usr/bin/bash
 
-script_version="0.1.02"    # prev - 0.0.08
-engine_version="1.0.8 Lua" # prev - 1.0.7 Marato
+script_version="0.1.03"    # prev - 0.1.02
+engine_version="1.0.9 Lua" # prev - 1.0.8 Lua
 
 # log
 loc=~/EchoMinal/; # default clone location
@@ -18,8 +18,8 @@ red='\033[1;91m'; deep_green='\033[0;32m'; green='\033[1;92m'; yellow='\033[1;93
 
 function UpdateEchominal() {
 
-  # EchoMinal Engine 1.0.8 Lua
-  # Replacing File List = engine.bash bashrc .scripts/update.bash .config/ccli.cfg 
+  # EchoMinal Engine 1.0.9 Lua
+  # Replacing File List = engine.bash 
 
   # getting user permission
   read -p "do u want to update [y/n] " update
@@ -27,19 +27,12 @@ function UpdateEchominal() {
   # condition
   if [[ $update == 'y' ]]; then {
 
-    # just for fun
+    # Simple Animation 
     copy
-
-    # installing packages
-    # apt install bat 
 
     # replaceing engine
     printf "Replacing Engine  : ";
     if cd $loc && cp -rv .engine.bash $HOME >> $log; then printf "Done\n"; fi;
-
-    # replaceing ccli-configuration
-    printf "Replacing CCLI    : ";
-    if cd $loc && cp -rv .config/ccli/termux.cfg $HOME/.config/ccli >> $log; then printf "Done\n"; fi;
       
     # removing messages
     printf "Replacing Engine  : ";
@@ -48,28 +41,6 @@ function UpdateEchominal() {
     }; else {
         echo "Failed to fixing zshrc"
     }; fi;
-
-    # replaceing bashrc
-    # printf "Copying Files     : ";
-    # if cd $loc && cp -rv .bashrc $HOME >> $log; then printf "Done\n"; fi;
-
-    read -p "are you rooted user? (y/n) " user
-    if [[ $user == 'y' ]]; then {
-        # termux - sudo 
-        echo -e "  \e[5mInstalling Termux sudo\e[0m"
-        printf "Cloning Git       : "
-        if git clone https://gitlab.com/st42/termux-sudo.git; then printf "Done\n"; fi
-        printf "Install a dependency needed for sudo\n"
-        if cd termux-sudo; then apt install ncurses-utils; fi;
-        printf "Execute the following commands to place sudo into the correct directory with the proper permissions and ownership\n";
-        cat sudo > /data/data/com.termux/files/usr/bin/sudo
-        chmod 700 /data/data/com.termux/files/usr/bin/sudo
-    }; elif [[ $user == 'n' ]]; then {
-        echo "you ar not rooted user";
-        echo "This update is not for you";
-    }; else {
-      echo "Invlid Options"
-    }; fi
 
     # deleting update script
     printf "Deleting Files    : ";
@@ -94,16 +65,14 @@ function UpdateChangeLog() {
 
   printf "\nHurrah\nNew update has been arrival $yellow\n"
   printf "============================$red\n"
-  printf "      EchoMinal 1.0.8 $yellow\n"
+  printf "      EchoMinal 1.0.9 $yellow\n"
+  printf "       Testing Update $yellow\n"
   printf "============================\n$red"
-  printf "*. New method of scripts manager on main menu\n"
-  printf "*. Spinner method installed on updating system\n"
-  printf "*. Bug fixes of engine\n"
-  printf "*. Added$blue 'bat'$red as installer\n"
-  printf "*. Added$blue 'Weeman'$red Script\n"
-  printf "*. added$blue 'sudo'$red super user do\n"
-  printf "*. added$blue 'aircrack-ng'$red script\n"
-  printf "*. added$blue 'hakku framwework$red\n"
+  printf "*. Install script has been updated\n"
+  printf "*. removed 'update zsh'\n"
+  printf "*. removed 'uninstall zsh'\n"
+  printf "*. fixed exit issue (working)\n"
+  printf "*. fixed ping request\n"
   printf "$stop\n"
 }
 
